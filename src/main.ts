9b41b11d5b8f9d89c03a250fd1d75013593a111e
AleChiarilli <alejandrochiarilli@gmail.com>
2023-09-14 16:47:01 +0200

    feat: decimal operations

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const signs = document.querySelectorAll(".sign");
 const equals = document.querySelector(".equals") as HTMLElement;
 const clear = document.querySelector(".clear") as HTMLElement;
 const negative = document.querySelector(".negative") as HTMLElement;
-const percent = document.querySelector(".percent");
+const percent = document.querySelector(".percent") as HTMLElement;
 
 const status = document.getElementById("calculator-status") as HTMLElement;
 
@@ -17,20 +17,22 @@ let operator = "";
 
 function updateCalculatorStatus() {
   return `${firstValue} ${operator} ${secondValue}`;
-}
+};
 
 numbers.forEach((n) => n.addEventListener('click', (event) => {
   const numberButton = event.currentTarget as HTMLElement;
   const value = numberButton.innerText;
   let displayedValue = "";
+  const isComma = value === ",";
   if (!operator) {
-    firstValue += value;
-    displayedValue = firstValue;
+    if (isComma && firstValue.includes(",")) return;
+      firstValue += value;
+      displayedValue = firstValue;
   } else {
-    secondValue += value;
-    displayedValue = secondValue;
+    if (isComma && secondValue.includes(",")) return;
+      secondValue += value;
+      displayedValue = secondValue;
   }
-
   result.innerHTML = displayedValue;
   status.innerHTML = updateCalculatorStatus();
 }));
@@ -42,9 +44,10 @@ signs.forEach((s) => s.addEventListener('click', (event) => {
   status.innerHTML = updateCalculatorStatus();
 }));
 
-function calculate(numberA: string, numberB: string, operatorSymbol: string) {
-  const valueA = Number(numberA);
-  const valueB = Number(numberB);
+function calculate(numberA: string, numberB: string, operatorSymbol: string) {  
+  const valueA = numberA.includes(',') ? parseFloat(numberA.replace(',' , '.')) : parseInt(numberA);
+  const valueB = numberB.includes(',') ? parseFloat(numberB.replace(',' , '.')) : parseInt(numberB);
+  
   switch (operatorSymbol) {
     case '+' :
       return valueA + valueB;
@@ -61,22 +64,19 @@ function calculate(numberA: string, numberB: string, operatorSymbol: string) {
     default: 
       return valueA;
   }
-}
-
+};
 
 equals.addEventListener('click', () => {
   if (!operator) return;
   const calculationResult = `${calculate(firstValue, secondValue, operator)
   }`;
-  result.innerHTML = calculationResult;
+  result.innerHTML = calculationResult.includes('.') ? calculationResult.replace('.' , ',') : calculationResult;
   firstValue = calculationResult;
   operator = "";
   secondValue= "";
   status.innerHTML = updateCalculatorStatus();
   status.innerHTML = "";
-  
-
-})
+});
 
 clear.addEventListener('click', () => {
   result.innerHTML = "0";
@@ -84,16 +84,16 @@ clear.addEventListener('click', () => {
   operator = "";
   firstValue = "";
   secondValue = "";
-})
+});
 
 negative.addEventListener('click', () => {
   const convert = parseInt(firstValue) * -1;
   firstValue = convert.toString();
   result.innerHTML = convert.toString();
-})
+});
 
-percent?.addEventListener('click', () => {
+percent.addEventListener('click', () => {
   const percent = parseFloat(firstValue) / 100
   firstValue = percent.toString();
   result.innerHTML = firstValue;
-})
+});
